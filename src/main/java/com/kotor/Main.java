b1650061e2ce469354c8e4867ddf04a8dccdb732
michaelkotor <milan3020@gmail.com>
2021-02-23 18:17:51 +0200

    edit

--- src/main/java/com/kotor/Main.java
+++ src/main/java/com/kotor/Main.java
@@ -2,6 +2,8 @@ package com.kotor;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Start");
         new Instruction().execute();
+        System.out.println("End");
     }
 }
