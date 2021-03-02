bf8b5f153d5366bb03c5596032aa7c68bb2455e0
michaelkotor <milan3020@gmail.com>
2021-03-02 18:31:09 +0200

    travis

--- src/main/java/com/kotor/Main.java
+++ src/main/java/com/kotor/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
         new Instruction().execute();
-        System.out.println("End!");
+        System.out.println("End!!");
     }
 }
